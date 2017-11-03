b40ff32e8a48073a3871d74978b476ddd911a81a
Ax3man <wouter.van.der.bijl@zoologi.su.se>
2017-11-03 11:46:50 +0100

    Update website.

--- docs/articles/binary_models.R
+++ docs/articles/binary_models.R
@@ -1,43 +1,40 @@
 ## ---- fig.align='center', fig.width=10, fig.height=8, out.height="600px", fig.dpi = 600----
 library(phylopath)
 
-models <- list(
-  A = DAG(C ~ M + P + G + D),
-  B = DAG(C ~ P + G + D,      M ~ M),
-  C = DAG(C ~ P + G + D,                P ~ M),
-  D = DAG(C ~ P + G + D,      M ~ P,               G ~ P),
-  E = DAG(C ~ P + G + D,                P ~ M,     G ~ P),
-  F = DAG(C ~ P + G + D,                P ~ M + G),
-  G = DAG(C ~ P + G + D,      M ~ P,    P ~ G),
-  H = DAG(C ~ P + G + D,      M ~ P),
-  I = DAG(C ~ P + G + D,      M ~ M,               G ~ P),
-  J = DAG(C ~ P + G,          M ~ P,               G ~ D),
-  K = DAG(C ~ P + G,                    P ~ M,     G ~ D),
-  L = DAG(C ~ M + P + G + D,            P ~ M + G)
+models <- define_model_set(
+  A = c(C~M+D),
+  B = c(C~D),
+  C = c(C~D, P~M),
+  D = c(C~D, M~P, G~P),
+  E = c(C~D, P~M, G~P),
+  F = c(C~D, P~M+G),
+  G = c(C~D, M~P, P~G),
+  H = c(C~D, M~P),
+  I = c(C~D, M~M, G~P),
+  J = c(M~P, G~D),
+  K = c(P~M, G~D),
+  L = c(C~M+D, P~M+G),
+  .common = c(C~P+G)
 )
 
-plot_model_set(models)
-
-## ---- eval=FALSE---------------------------------------------------------
-#  cichlids_results <- phylo_path_binary(models, cichlids, cichlids_tree, parallel = "SOCK")
-
-## ---- echo=FALSE---------------------------------------------------------
-message("15 rows were dropped because they contained NA values.")
-message("Pruned tree to drop species not included in dat.")
-cichlids_results <- phylopath:::cichlids_results
+plot_model_set(models, algorithm = 'kk')
 
 ## ------------------------------------------------------------------------
-summary(cichlids_results)
+(cichlids_results <- phylo_path(models, cichlids, cichlids_tree))
 
-## ---- eval=FALSE---------------------------------------------------------
-#  best_cichlids <- best(cichlid_results)
+## ------------------------------------------------------------------------
+(s <- summary(cichlids_results))
+plot(s)
 
-## ---- echo=FALSE---------------------------------------------------------
-best_cichlids <- phylopath:::best_cichlids
+## ------------------------------------------------------------------------
+best_cichlids <- best(cichlids_results)
 
 ## ------------------------------------------------------------------------
 best_cichlids
 
+## ------------------------------------------------------------------------
+coef_plot(best_cichlids, error_bar = "se", reverse_order = TRUE) + ggplot2::coord_flip()
+
 ## ---- fig.align='center', fig.width=8, fig.height=4, out.width="600px", fig.dpi = 300----
-plot(best_cichlids, algorithm = 'kk')
+plot(best_cichlids)
 
--- docs/articles/intro_to_phylopath.R
+++ docs/articles/intro_to_phylopath.R
@@ -1,19 +1,32 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  library(ape)
+#  my_tree <- read.tree('my_tree.tre') # For Newick format trees
+#  my_tree <- read.nexus('my_tree.nex') # For NEXUS format trees
+
+## ---- eval=FALSE---------------------------------------------------------
+#  rownames(my_data) <- my_data$species_name
+
+## ---- eval=FALSE---------------------------------------------------------
+#  my_tree$tip.label # Check the tip labels of your tree
+#  rownames(my_data) <- gsub(' ', '_', my_data$species_name_with_spaces)
+
 ## ---- include = FALSE----------------------------------------------------
 knitr::opts_chunk$set(dev = "png", fig.height = 5, fig.width = 5, dpi = 300, out.width = "450px")
 
 ## ------------------------------------------------------------------------
 library(phylopath)
 
-models <- list(
-  one   = DAG(LS ~ BM, NL ~ BM, DD ~ NL, RS ~ DD),
-  two   = DAG(LS ~ BM, NL ~ BM, DD ~ NL, RS ~ LS + DD),
-  three = DAG(LS ~ BM, NL ~ BM, DD ~ NL, RS ~ NL),
-  four  = DAG(LS ~ BM, NL ~ BM, DD ~ NL, RS ~ BM + NL),
-  five  = DAG(LS ~ BM, NL ~ BM, DD ~ NL, RS ~ BM + NL + DD),
-  six   = DAG(LS ~ BM, NL ~ BM + RS, DD ~ NL, RS ~ BM),
-  seven = DAG(LS ~ BM, NL ~ BM + RS, DD ~ NL, RS ~ LS + BM),
-  eight = DAG(LS ~ BM, NL ~ BM + RS, DD ~ NL),
-  nine  = DAG(LS ~ BM, NL ~ BM + RS, DD ~ NL, RS ~ LS)
+models <- define_model_set(
+  one   = c(RS ~ DD),
+  two   = c(DD ~ NL, RS ~ LS + DD),
+  three = c(RS ~ NL),
+  four  = c(RS ~ BM + NL),
+  five  = c(RS ~ BM + NL + DD),
+  six   = c(NL ~ RS, RS ~ BM),
+  seven = c(NL ~ RS, RS ~ LS + BM),
+  eight = c(NL ~ RS),
+  nine  = c(NL ~ RS, RS ~ LS),
+  .common = c(LS ~ BM, NL ~ BM, DD ~ NL)
 )
 
 ## ------------------------------------------------------------------------
@@ -26,18 +39,39 @@ plot(models$one)
 plot_model_set(models)
 
 ## ------------------------------------------------------------------------
-result <- phylo_path(models, data = rhino, tree = rhino_tree, 
-                     order = c('BM', 'NL', 'DD', 'LS', 'RS'))
+result <- phylo_path(models, data = rhino, tree = rhino_tree, model = 'lambda')
 
 ## ------------------------------------------------------------------------
 result
 
 ## ------------------------------------------------------------------------
-summary(result)
+(s <- summary(result))
+
+## ------------------------------------------------------------------------
+plot(s)
 
 ## ------------------------------------------------------------------------
 (best_model <- best(result))
 
 ## ---- warning = FALSE, fig.width = 6-------------------------------------
 plot(best_model)
+
+## ---- fig.width = 7------------------------------------------------------
+average_model <- average(result)
+plot(average_model, algorithm = 'mds', curvature = 0.1) # increase the curvature to avoid overlapping edges
+
+## ---- fig.width = 7------------------------------------------------------
+average_model_full <- average(result, avg_method = "full")
+plot(average_model_full, algorithm = 'mds', curvature = 0.1)
+
+## ------------------------------------------------------------------------
+#coef_plot(average_model)
+
+## ---- fig.height=3.5-----------------------------------------------------
+# coef_plot(average_model_full, reverse_order = TRUE) + 
+#   ggplot2::coord_flip() + 
+#   ggplot2::theme_bw()
+
+## ------------------------------------------------------------------------
+result$d_sep$one
 
